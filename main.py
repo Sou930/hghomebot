588223543dc2dacffb99906fe0a540744f7e3816
Sou930 <69160@g.hiroshimagakuin.ed.jp>
2025-10-04 13:39:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from discord import app_commands
 from keep_alive import keep_alive
 from program.count import CountCog
 from program.base import BaseCog
-from program.weather import EarthquakeCog
 from program.currency import Currency
 
 TOKEN = os.environ.get("DISCORD_TOKEN")  # Renderの環境変数で設定する
@@ -29,7 +28,6 @@ async def on_ready():
 async def setup():
     await bot.add_cog(CountCog(bot))
     await bot.add_cog(BaseCog(bot))
-    await bot.add_cog(EarthquakeCog(bot))
     await bot.add_cog(Currency(bot))
     
 async def main():
